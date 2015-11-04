e4ce47846c93af8e9e8a76da28e8d819b2733142
Max Martin <maxmartin360@gmail.com>
2015-11-03 20:55:32 -0500

    initial upload of portfolio

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+$(document).ready( function() {
+
+	$('#facebook, #instagram, #linkedin, #email,').hover(function(){
+			$(this).css('color', '#236170');
+			}, function() {
+			$(this).css('color', '#7f8c8d');	
+		});
+
+});
